a71d0dda6475c11c567d2aa2eb8dff9a734d72b1
Matthias Grosser <mtgrosser@gmx.net>
2022-10-29 17:46:03 +0200

    Digispark branch

--- acs712_i2t.ino
+++ acs712_i2t.ino
@@ -6,17 +6,21 @@
 #define RMS_FACTOR      0.707106
 #define RMS_WINDOW      50  // ms
 #define AVERAGE_SAMPLES 5.0
-#define SENSITIVITY     0.120 // mv/A
-#define SENSOR_BIAS     0.25  // A
+#define SENSITIVITY     0.130 // mv/A
+#define SENSOR_BIAS     -0.05 // A
 #define I_NOMINAL       3.0 // A
 #define I_OVER          5.0 // A
 #define T_OVER          1000 // ms
 #define S_I2T           ((I_OVER * I_OVER - I_NOMINAL * I_NOMINAL) * T_OVER) // A^2ms
 #define TRIP_DURATION   1000 // ms
-#define LED_GREEN       2
-#define LED_YELLOW      3
-#define LED_RED         4
-#define RELAY           6
+#define A_SENSE         1  // PB2
+#define A_GND           2  // P4
+#define A_VCC           3  // P3
+#define LED             1  // PB1
+//#define LED_GREEN       2
+//#define LED_YELLOW      3
+//#define LED_RED         4
+#define RELAY           1  // PB1
 
 
 float Iavg = 0;
@@ -36,9 +40,9 @@ void updateMovingAverage(float fValue) {
 
 
 float measure() {
-  int iRefGnd = analogRead(A0);
-  int iRefVcc = analogRead(A1);
-  int iSense  = analogRead(A2);
+  int iRefGnd = 0; //analogRead(A_GND);
+  int iRefVcc = 1023; //analogRead(A_VCC);
+  int iSense  = analogRead(A_SENSE);
 
   float fVcc = (iRefVcc + 0.5) * 5.0 / 1024.0;
   float fSense = (iSense + 0.5) * 5.0 / 1024.0;
@@ -48,11 +52,15 @@ float measure() {
 
 
 void setup() {
-  pinMode(LED_GREEN, OUTPUT);
-  pinMode(LED_YELLOW, OUTPUT);
-  pinMode(LED_RED, OUTPUT);
+  pinMode(2, INPUT); // A1 = PB2
+  //pinMode(3, INPUT); // A3
+  //pinMode(4, INPUT); // A0
+  pinMode(LED, OUTPUT);
+  //pinMode(LED_GREEN, OUTPUT);
+  //pinMode(LED_YELLOW, OUTPUT);
+  //pinMode(LED_RED, OUTPUT);
   pinMode(RELAY, OUTPUT);
-  Serial.begin(9600);
+  //Serial.begin(9600);
 }
 
 
@@ -79,6 +87,9 @@ void loop() {
 
   // debug only
   if (ulCurrentMillis % 500 == 0) {
+    //digitalWrite(RELAY, random(10) > 5 ? HIGH : LOW);
+    //digitalWrite(LED, random(10) > 5 ? HIGH : LOW);
+    /*
     Serial.print("Irms: ");
     Serial.println(Irms, 1);
     Serial.print("SI2t: ");
@@ -86,8 +97,9 @@ void loop() {
     Serial.print("sfI2t: ");
     Serial.println(sfI2t, 0);
     Serial.println();  
-  }
-
+  */
+}
+  
   if (ulDt < RMS_WINDOW) return;
   
   sulLastMillis = ulCurrentMillis;
@@ -111,8 +123,9 @@ void loop() {
     }
   }
 
-  digitalWrite(RELAY, bPower ? LOW : HIGH);
-  digitalWrite(LED_GREEN, Irms > 1.0 ? HIGH : LOW);
-  digitalWrite(LED_YELLOW, Irms > 2.0 ? HIGH : LOW);
-  digitalWrite(LED_RED, bTrip ? HIGH : LOW);
+  digitalWrite(RELAY, bPower ? HIGH : LOW);
+  //digitalWrite(LED_GREEN, Irms > 1.0 ? HIGH : LOW);
+  //digitalWrite(LED_YELLOW, Irms > 2.0 ? HIGH : LOW);
+  //digitalWrite(LED_RED, bTrip ? HIGH : LOW);
+  //digitalWrite(LED, Irms > 2.0 ? HIGH : LOW);
 }
