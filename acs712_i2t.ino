bbc15d4d69cc17bd0d272d438ee126b19767dfc8
Matthias Grosser <mtgrosser@users.noreply.github.com>
2022-11-03 02:45:22 +0100

    Update acs712_i2t.ino

--- acs712_i2t.ino
+++ acs712_i2t.ino
@@ -3,24 +3,24 @@
  * 
  */
 
-#define RMS_FACTOR      0.707106
-#define RMS_WINDOW      50  // ms
-#define AVERAGE_SAMPLES 5.0
-#define SENSITIVITY     0.130 // mv/A
-#define SENSOR_BIAS     -0.05 // A
-#define I_NOMINAL       3.0 // A
-#define I_OVER          5.0 // A
-#define T_OVER          1000 // ms
-#define S_I2T           ((I_OVER * I_OVER - I_NOMINAL * I_NOMINAL) * T_OVER) // A^2ms
-#define TRIP_DURATION   1000 // ms
-#define A_SENSE         1  // PB2
-#define A_GND           2  // P4
-#define A_VCC           3  // P3
-#define LED             1  // PB1
-//#define LED_GREEN       2
-//#define LED_YELLOW      3
-//#define LED_RED         4
-#define RELAY           1  // PB1
+#define RMS_FACTOR       0.707106
+#define RMS_WINDOW       50  // ms
+#define AVERAGE_SAMPLES  5.0
+#define U_REF            5000.0  // mV
+#define ADC_UNIT         4.88281 // mV
+#define ADC_ZERO         509
+#define SENSITIVITY      170.0 // mv/A
+#define I_NOMINAL        3.25  // A
+#define I_OVER           5.0   // A
+#define T_OVER           1000  // ms
+#define S_I2T            ((I_OVER * I_OVER - I_NOMINAL * I_NOMINAL) * T_OVER) // A^2ms
+#define TRIP_DURATION    1000 // ms
+#define RETRIP_DURATION  100  // ms
+#define RETRIP_MAX_COUNT 4
+#define RETRIP_RESET     5000 // ms
+#define PIN_SENSE        1  // PB2
+#define PIN_LED          1  // PB1
+#define PIN_RELAY        0  // PB0
 
 
 float Iavg = 0;
@@ -28,7 +28,7 @@ float Irms = 0;
 
 
 void updateMovingAverage(float fValue) {
-  static boolean sbInitialized = false;
+  static bool sbInitialized = false;
 
   if (!sbInitialized) {
     Iavg = fValue;
@@ -40,14 +40,8 @@ void updateMovingAverage(float fValue) {
 
 
 float measure() {
-  int iRefGnd = 0; //analogRead(A_GND);
-  int iRefVcc = 1023; //analogRead(A_VCC);
-  int iSense  = analogRead(A_SENSE);
-
-  float fVcc = (iRefVcc + 0.5) * 5.0 / 1024.0;
-  float fSense = (iSense + 0.5) * 5.0 / 1024.0;
-  
-  return (fSense - fVcc / 2.0) / SENSITIVITY - SENSOR_BIAS;
+  int iSense = analogRead(PIN_SENSE) - ADC_ZERO;
+  return (ADC_UNIT * (float)iSense) / SENSITIVITY;
 }
 
 
@@ -55,41 +49,42 @@ void setup() {
   pinMode(2, INPUT); // A1 = PB2
   //pinMode(3, INPUT); // A3
   //pinMode(4, INPUT); // A0
-  pinMode(LED, OUTPUT);
-  //pinMode(LED_GREEN, OUTPUT);
-  //pinMode(LED_YELLOW, OUTPUT);
-  //pinMode(LED_RED, OUTPUT);
-  pinMode(RELAY, OUTPUT);
+  pinMode(PIN_RELAY, OUTPUT);
+  pinMode(PIN_LED, OUTPUT);
   //Serial.begin(9600);
 }
 
 
 void loop() {
-  static float sfImax = 0;
-  static float sfLastImax = 0;
-  static float sfI2t = 0;
-  static unsigned long sulLastMillis = millis();
-  static unsigned long sulTripStart = 0;
-  //static unsigned long sulPreviousMillis = 0;
+  static float sImax = 0;
+  static float sLastImax = 0;
+  static float sI2t = 0;
+  static unsigned long sLastMillis = millis();
+  static unsigned long sTripStart = 0;
+  static unsigned long sTripReset = 0;
+  static unsigned int sRetripCount = 0;
 
-  float fI, fI2t;
-  boolean bTrip, bPower;
-  unsigned long ulCurrentMillis = millis();
-  unsigned long ulDt = ulCurrentMillis - sulLastMillis;
+  float I, Iabs, I2t;
+  boolean trip, power;
+  unsigned long powerDuration = 0;
+  unsigned long currentMillis = millis();
+  unsigned long dt = currentMillis - sLastMillis;
 
-  fI = measure();
-  fI = abs(fI);
-  updateMovingAverage(fI);
+  I = measure();
+  updateMovingAverage(I);
 
-  if (Iavg > sfImax) {
-    sfImax = Iavg;
+  Iabs = abs(Iavg);
+
+  if (Iabs > sImax) {
+    sImax = Iabs;
   }
 
   // debug only
+  /*
   if (ulCurrentMillis % 500 == 0) {
+    Serial.println(Irms, 2);
     //digitalWrite(RELAY, random(10) > 5 ? HIGH : LOW);
     //digitalWrite(LED, random(10) > 5 ? HIGH : LOW);
-    /*
     Serial.print("Irms: ");
     Serial.println(Irms, 1);
     Serial.print("SI2t: ");
@@ -97,35 +92,37 @@ void loop() {
     Serial.print("sfI2t: ");
     Serial.println(sfI2t, 0);
     Serial.println();  
+  }
   */
-}
   
-  if (ulDt < RMS_WINDOW) return;
+  if (dt < RMS_WINDOW) return;
   
-  sulLastMillis = ulCurrentMillis;
-  sfLastImax = sfImax;
-  sfImax = 0;
-  Irms = sfLastImax * RMS_FACTOR;
-    
-  fI2t = (Irms * Irms - I_NOMINAL * I_NOMINAL) * (float)ulDt;
-  sfI2t += fI2t;
-  if (sfI2t < 0) sfI2t = 0;
+  sLastMillis = currentMillis;
+  sLastImax = sImax;
+  sImax = 0;
+  Irms = sLastImax * RMS_FACTOR;
+  
+  I2t = (Irms * Irms - I_NOMINAL * I_NOMINAL) * (float)dt;
+  sI2t += I2t;
+  if (sI2t < 0) sI2t = 0;
 
-  bTrip = sfI2t > S_I2T;
+  trip = sI2t > S_I2T;
+  powerDuration = currentMillis - sTripReset;
 
-  if (bTrip) {
-    sulTripStart = ulCurrentMillis;
-    bPower = false;
+  if (trip) {
+    sTripStart = currentMillis;
+    power = false;
+    if (powerDuration < RETRIP_DURATION && sRetripCount < RETRIP_MAX_COUNT)
+      sRetripCount++;
   } else {
-    if (ulCurrentMillis - sulTripStart > TRIP_DURATION) {
-      bPower = true;
-      sulTripStart = 0;
+    if (currentMillis - sTripStart > pow(2, sRetripCount) * TRIP_DURATION) {
+      power = true;
+      if (powerDuration > RETRIP_RESET) sRetripCount = 0;
+      sTripReset = currentMillis;
+      sTripStart = 0;
     }
   }
-
-  digitalWrite(RELAY, bPower ? HIGH : LOW);
-  //digitalWrite(LED_GREEN, Irms > 1.0 ? HIGH : LOW);
-  //digitalWrite(LED_YELLOW, Irms > 2.0 ? HIGH : LOW);
-  //digitalWrite(LED_RED, bTrip ? HIGH : LOW);
-  //digitalWrite(LED, Irms > 2.0 ? HIGH : LOW);
+  
+  digitalWrite(PIN_LED, power ? HIGH : LOW);
+  digitalWrite(PIN_RELAY, power ? HIGH : LOW);
 }
